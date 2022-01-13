dd2587cc19d5d2c828119715e88be8cbc534182f
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2022-01-13 13:50:23 -0800

    modifique la barralateral

--- src/Barralateral/Barralateral.jsx
+++ src/Barralateral/Barralateral.jsx
@@ -1,8 +1,40 @@
-import { Navegation, Link } from "./Index"
-export default function Barralateral(){
+
+import { Fragment } from "react";
+import { Navegation} from "./Index";
+import MediaQuery from "react-responsive";
+
+import {If, Then, Else} from 'react-if';
+
+import LinkComponents from './LinkComponents';
+import LinkCompletoComponents from './LinkCompletoComponents';
+import LinkCompletoMovileComponents from './LinkCompletoMovileComponents'
+
+export default function Barralateral(props){
+
     return (
-        <Navegation>
-            {/* <Link href="/">hola</Link> */}
-        </Navegation>
+        <Fragment>
+            <MediaQuery minWidth={1313}>
+                <Navegation align="flex-start" ancho={props.stadoLateral ?  "240" : "72"}>
+                    <If condition={props.stadoLateral == false}>
+                        <Then>
+                            <LinkComponents/>
+                        </Then>
+                        <Else>
+                            <LinkCompletoComponents/>
+                        </Else>
+                    </If>
+                </Navegation>
+            </MediaQuery>
+
+            <MediaQuery maxWidth={1312}>
+                <Navegation align="flex-start" ancho="72">
+                    <LinkComponents/>
+                    <LinkCompletoMovileComponents stadoLateral={props.stadoLateral}/>
+                </Navegation>
+            </MediaQuery>
+            
+
+        </Fragment>
+        
     );
 }
