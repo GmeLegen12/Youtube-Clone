a12a51a981e4cd5abef3f1006b6bc836cb669d88
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2022-01-13 13:49:46 -0800

    modifique el app y le puse los estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { Fragment } from 'react';
-import '../assets/App.css'
-import Router from './Router'
-import Cabezera from './Cabezera/Cabazera'
-import Barralateral from "./Barralateral/Barralateral"
+import { Fragment, useState } from 'react';
+import '../assets/App.css';
+
+import Router from './Router';
+import Cabezera from './Cabezera/Cabazera';
+import Barralateral from "./Barralateral/Barralateral";
 function App() {
+  const [ abrirocerrarLateral, stadodeBarra ] = useState(true)
+
   return (
     <Fragment>
-        <Cabezera/>
-        <Barralateral/>
+        <Cabezera abrirLateral={() => abrirocerrarLateral ? stadodeBarra(false) : stadodeBarra(true)} />
+        <Barralateral stadoLateral={abrirocerrarLateral} />
         {/* <main>
           <Router/>
         </main> */}
