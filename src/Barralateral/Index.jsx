5b703d9c8a663609561c9b1365ed4528f5a2897c
Dylan Wilfrido Guzman Quintero <dylanwguzmanq@outlook.com>
2022-01-13 13:50:53 -0800

    Agrege y modifique el estilos de algunos componentes

--- src/Barralateral/Index.jsx
+++ src/Barralateral/Index.jsx
@@ -1,13 +1,50 @@
 import styled from "styled-components";
 
 export const Navegation = styled.nav`
-    width: 72px;
+    width: ${props => props.ancho + "px"};
     height: 100vh;
     background-color: #fff;
-    display: fixed;
+    flex-direction: column;
+    display: flex;
+    font-size: 16px;
+    align-items:  ${props => props.align ||"center"};
+    position: relative;
+    transition: width 250ms cubic-bezier(0, 1.83, 0.99, 0.76);
+`;
+
+export const Contedor = styled.div`
+    width: 202px;
+    height: 100%;
+    background: #fff;
+    position: ${props => props.position};
+    margin-left: ${props => props.left + "px"};
+    transition: margin-left 300ms cubic-bezier(1, -0.04, 0, 1.1);
+`;
+
+export const Icon = styled.i`
+    font-size: ${props => props.sizeF + "px" || "10px"};
+    color: ${props => props.colorF};
 `;
 
 export const Link = styled.a`
-
+    background-color: 
+        background-color: ${props => props.Background || "#fff"};
+    width: ${props => props.ancho + "px" || "69%"};
+    height: 49px;
+    display: flex;
+    flex-direction: ${props => props.direc || "column"};
+    align-items: center;
+    margin-top: 20px;
+    margin-left: ${props => props.left + "px"};
+    transition: background 300ms ease-in;
+    border-radius: 50%;
+    &:hover {
+        background-color: ${props => props.BackHover || "#d4d4d4"};
+        color: ${props => props.ColorHover };
+    }
 `;
-
+export const H2 = styled.h2`
+    font-size: ${props => props.sizeF + "px" || "10px"};
+    color: ${props => props.colorF};
+    margin-left: ${props => props.left + "px"};
+`;
